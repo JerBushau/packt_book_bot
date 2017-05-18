78dffe1ae9d25939ac1522a5cd158144bec72164
JerBushau <exjerlock@hotmail.com>
2017-05-18 07:08:09 -0400

    Added link to pack in mssg

--- packtbot.js
+++ packtbot.js
@@ -14,7 +14,7 @@ module.exports = function (req, res, next) {
     const $ = cheerio.load(body);
     const freeBook = $('.dotd-title h2').text().trim();
     const botPayload = {
-      text : `Today's free book is "${freeBook}".`
+      text : `Today's free book is "${freeBook}" https://www.packtpub.com/packt/offers/free-learning/.`
     };
 
     return res.status(200).json(botPayload);
