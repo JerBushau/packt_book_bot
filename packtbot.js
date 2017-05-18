3cbdfa552a8504f29be1c2196df2ee60a2c0562b
JerBushau <exjerlock@hotmail.com>
2017-05-18 17:39:51 -0400

    Changed message

--- packtbot.js
+++ packtbot.js
@@ -6,7 +6,7 @@ const packt = "https://www.packtpub.com/packt/offers/free-learning/";
 
 module.exports = function (req, res, next) {
 
-  request(packt, (error, response, body) => {
+  request(packt, function (error, response, body) {
     if (error) {
       return console.error(`error:  ${error}`);
     }
@@ -15,7 +15,7 @@ module.exports = function (req, res, next) {
     const freeBook = $('.dotd-title h2').text().trim();
     const botPayload = {
       response_type: "in_channel",
-      text : `Today's free book is "${freeBook}". https://www.packtpub.com/packt/offers/free-learning/`
+      text : `Today's free book is "${freeBook}". \n:point_right: ${packt}`
     };
 
     return res.status(200).json(botPayload);
